5cc33b1a3cddcad867e9357b7ee0ca5f40eab283
asadiqbalkhan <iq.asad@gmail.com>
2021-07-01 14:26:54 +0500

    Image POST & GET done through IPFS , add smart contract logic now

--- demo/src/App.js
+++ demo/src/App.js
@@ -4,37 +4,13 @@ import React, { Component } from 'react';
 // ipfs client
 const ipfsClient = require('ipfs-http-client')
 const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' }) // leaving out the arguments will default to these values
-// ================
-// INFURA SETTINGS 
-// ================
-// const { create } = require('ipfs-http-client')
-
-// const projectId = '1uhgp0Dgz9Ygv2owxQQcQdO6nDN'
-// const projectSecret = '3aec14ed5d6d4b2235c90a7f1654c178'
-// const auth =
-//   'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
-
-// const ipfs = create({
-//   host: 'ipfs.infura.io',
-//   port: 5001,
-//   protocol: 'https',
-//   headers: {
-//     authorization: auth
-//   }
-// })
-
-// ipfs.pin.add('QmeGAVddnBSnKc1DLE7DLV9uuTqo5F7QbaveTjr45JUdQn').then((res) => {
-//   console.log(res)
-// })
-
-
-
 class App extends Component{
 
   constructor(props){
     super(props);
     this.state = {
-      buffer: null
+      buffer: null,
+      imageHash: 'QmUfedgqKVRbcuuwfQ2uEiDEfHhS882DnYXDZ7gHp5Yxsq'
     };
   }
 
@@ -52,7 +28,8 @@ class App extends Component{
     }
     
   }
-
+  // Example : hash: "QmUfedgqKVRbcuuwfQ2uEiDEfHhS882DnYXDZ7gHp5Yxsq"
+  // Example: url: https://ipfs.infura.io/ipfs/QmUfedgqKVRbcuuwfQ2uEiDEfHhS882DnYXDZ7gHp5Yxsq
   onSubmit = (event) => {
     event.preventDefault()
     console.log('Submitting the form')
@@ -60,10 +37,13 @@ class App extends Component{
     ipfs.add(this.state.buffer , (error, result) => {
       //do stuff here
       console.log('IPFS result', result)
+      const imageHash = result[0].hash
+      this.setState({ imageHash  })
       if(error){
         console.error(error)
         return
       }
+      // Step 2 store a file on blockchain...
     })
   }
 
@@ -84,7 +64,7 @@ class App extends Component{
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <img src = '' className="logo" alt="logo"/>
+                <img src={`https://ipfs.infura.io/ipfs/${this.state.imageHash}`} alt='logo' />
                 <p>&nbsp;</p>
                 <h2>Change Meme</h2>
                 <form action="" onSubmit={this.onSubmit}>
